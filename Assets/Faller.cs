9abc0588a66b6c8d16df8cce0f0f674e3f4c0963
iuhmirza <iuhmirza@gmail.com>
2023-02-28 13:08:45 +0000

    Change effect of gravity and add rotation

--- Assets/Faller.cs
+++ Assets/Faller.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class Faller : MonoBehaviour
 {
     Rigidbody2D body;
-    float screenHeightInWorldUnits;
-    // Start is called before the first frame update
+    Vector2 screenHalfSizeWorldUnits;
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        screenHeightInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize;
+        screenHalfSizeWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 0, 1);
-        if (transform.position.y < -screenHeightInWorldUnits)
+        if (transform.position.y < -screenHalfSizeWorldUnits.y - 0.5f)
         {
             Destroy(gameObject);
         }
