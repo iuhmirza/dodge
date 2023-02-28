bd12a91c80e2630453b8104ab53d9d4a10034b02
iuhmirza <iuhmirza@gmail.com>
2023-02-28 12:27:53 +0000

    Add wrap around to player collision

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,12 +7,15 @@ public class Player : MonoBehaviour
     Rigidbody2D body;
     public float speed = 1;
     Vector2 velocity;
+    float screenHalfWidthInWorldUnits;
     
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        float halfPlayerWidth = transform.localScale.x / 2f;
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
     }
 
     // Update is called once per frame
@@ -26,13 +29,13 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         body.position += velocity * Time.fixedDeltaTime;
-        if (body.position.x > 7.5)
+        if (body.position.x > screenHalfWidthInWorldUnits)
         {
-            body.position = new Vector2(-7.5f, -3f);
+            body.position = new Vector2(-screenHalfWidthInWorldUnits, -3f);
         }
-        if (body.position.x < -7.5)
+        if (body.position.x < -screenHalfWidthInWorldUnits)
         {
-            body.position = new Vector2(7.5f, -3f);
+            body.position = new Vector2(screenHalfWidthInWorldUnits, -3f);
         }
     }
 }
