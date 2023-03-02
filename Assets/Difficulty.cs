527775936bb86e62703450dacaaacbefbb2775d2
iuhmirza <iuhmirza@gmail.com>
2023-03-02 14:15:42 +0000

    Refactor difficulty into seperate class

--- Assets/Difficulty.cs
+++ Assets/Difficulty.cs
@@ -2,12 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Difficulty
+public class Difficulty : MonoBehaviour
 {
-    static float secondsToMaxDifficulty = 1080;
+    public float spawnDelay;
+    public float maxSpawnSize;
+    public float gravityScale;
 
-    public static float GetDifficultyPercent()
+    public Vector2 screenSize;
+
+    float nextDifficultyIncrease;
+    float nextDifficultyTimer;
+
+    private void Start()
     {
-        return Mathf.Clamp01(Time.time / secondsToMaxDifficulty);
+        screenSize = new Vector2(Camera.main.aspect * Camera.main.orthographicSize * 2, Camera.main.orthographicSize * 2);
+        spawnDelay = 2 / screenSize.x;
+        gravityScale = screenSize.x / screenSize.y; // ratio is no good!
+        maxSpawnSize = 0.1f * screenSize.x;
+        nextDifficultyIncrease = 10f;
+        nextDifficultyTimer = Time.time;
     }
+
+    private void FixedUpdate()
+    {
+        if (Time.time > nextDifficultyTimer + nextDifficultyIncrease)
+        {
+            int choice = Random.Range(0, 3);
+            switch (choice)
+            {
+                case 0:
+                    gravityScale = Mathf.Min(gravityScale + 0.1f, 2f);
+                    break;
+                case 1:
+                    maxSpawnSize = Mathf.Min(maxSpawnSize + 0.1f, screenSize.x / 5);
+                    break;
+                case 2:
+                    spawnDelay = Mathf.Max(spawnDelay - 0.1f, 0.1f);
+                    break;
+
+            }
+            nextDifficultyTimer = Time.time;
+        }
+    }
+
 }
--- Assets/FallerSpawner.cs
+++ Assets/FallerSpawner.cs
@@ -5,50 +5,45 @@ using UnityEngine;
 public class FallerSpawner : MonoBehaviour
 {
     public GameObject fallerPrefab;
-    Vector2 screenHalfSizeWorldUnits;
-    float spawnDelay;
-    float nextDifficultyIncrease;
-    float maxSpawnSize;
-    float gravity;
+    public Difficulty difficulty;
 
     // Start is called before the first frame update
     void Start()
     {
-        screenHalfSizeWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
-        spawnDelay = 1 / screenHalfSizeWorldUnits.x * 2;
-        InvokeRepeating("SpawnFaller", 0, spawnDelay);
-        nextDifficultyIncrease = Time.time + 10f;
-        maxSpawnSize = 1.5f;
-        gravity = 1f;
+        StartCoroutine(Spawn());
         FindObjectOfType<GameOver>().TheGameIsOver += OnGameOver;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Time.time > nextDifficultyIncrease)
-        {
-            nextDifficultyIncrease = 2 * Time.time;
-            maxSpawnSize = Mathf.Min(maxSpawnSize + 0.5f, screenHalfSizeWorldUnits.x);
-            gravity += 0.2f;
-        }
     }
 
     void SpawnFaller()
     {
-        float randomSpawnSize = Random.Range(0.5f, maxSpawnSize);
-        Vector2 randomSpawnPosition = new Vector2(Random.Range(-screenHalfSizeWorldUnits.x, screenHalfSizeWorldUnits.x), screenHalfSizeWorldUnits.y + randomSpawnSize);
+        float randomSpawnSize = Random.Range(0.5f, difficulty.maxSpawnSize);
+        
+        Vector2 randomSpawnPosition = new Vector2(
+            Random.Range(-difficulty.screenSize.x / 2f, difficulty.screenSize.x / 2f),
+            difficulty.screenSize.y / 2f + randomSpawnSize);
+
         Vector3 randomSpawnRotation = Vector3.forward * Random.Range(0, 59);
 
         GameObject newFaller = (GameObject)Instantiate(fallerPrefab, randomSpawnPosition, Quaternion.Euler(randomSpawnRotation));
-        newFaller.transform.localScale = Vector2.one * randomSpawnSize;
-        newFaller.GetComponent<Rigidbody2D>().mass = randomSpawnSize;
-        newFaller.GetComponent<Rigidbody2D>().gravityScale = gravity;
+
+        Rigidbody2D newFallerBody = newFaller.GetComponent<Rigidbody2D>();
+        newFallerBody.transform.localScale = Vector2.one * randomSpawnSize;
+        newFallerBody.mass = randomSpawnSize;
+        newFallerBody.gravityScale = difficulty.gravityScale;
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            SpawnFaller();
+            yield return new WaitForSeconds(difficulty.spawnDelay);
+        }
     }
 
     void OnGameOver()
     {
-        gravity = 0;
+        difficulty.gravityScale = 0;
     }
 
 }
