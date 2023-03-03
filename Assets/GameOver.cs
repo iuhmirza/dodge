01746c0604e304f7b57c20481b79298252e807c9
iuhmirza <iuhmirza@gmail.com>
2023-03-03 18:59:54 +0000

    Rewrite game over with coroutine

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
-    public event System.Action TheGameIsOver;
+    public event System.Action Over;
+    
     public GameObject gameOverScreen;
     public TextMeshProUGUI distance;
+    public GameObject image;
+    
     bool gameOver;
     float elapsedTime;
     public float holdThrow = 0.1f;
@@ -30,7 +34,7 @@ public class GameOver : MonoBehaviour
             {
                 holdTimer -= Time.deltaTime;
                 if (holdTimer < 0)
-                    SceneManager.LoadScene(0);
+                    SceneManager.LoadScene(1);
                     elapsedTime = Time.time;
             }
             else
@@ -41,8 +45,23 @@ public class GameOver : MonoBehaviour
     void OnGameOver()
     {
         distance.text = ((int)(Time.time - elapsedTime)).ToString() + "m";
+        StartCoroutine(DisplayGameOverScreen());
+        Over();
+    }
+
+    IEnumerator DisplayGameOverScreen()
+    {
+
+        image.transform.localScale = new Vector2(Camera.main.aspect * Camera.main.orthographicSize * 2, Camera.main.orthographicSize * 2);
+        yield return new WaitForSeconds(2f);
+        for (int i = 0; i < 9; i++)
+        {
+            image.GetComponent<Image>().color = new Color(0, 0, 0, i / 10f);
+            yield return new WaitForSeconds(0.1f);
+        }
+
         gameOverScreen.SetActive(true);
-        TheGameIsOver();
         gameOver = true;
+
     }
 }
